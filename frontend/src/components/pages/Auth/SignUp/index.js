4a1bbd8e89c390b013a81a5ad3804a196d996337
lucassodresa <38632174+lucassodresa@users.noreply.github.com>
2022-04-10 00:07:07 +0100

    Revert "Revert "migration to RCA""

--- frontend/src/components/pages/Auth/SignUp/index.js
+++ frontend/src/components/pages/Auth/SignUp/index.js
@@ -24,8 +24,7 @@ const SignUp = () => {
     mode: 'onChange'
   });
 
-  const api = useAxios();
-
+  const { api } = useAxios();
   const { mutate, isLoading } = useMutation(authService.signUp(api), {
     onSuccess: ({ data }) => {
       navigate('/signin');
